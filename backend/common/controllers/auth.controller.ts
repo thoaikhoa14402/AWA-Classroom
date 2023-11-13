d21ce52689c603166c75a8798e4ad096673d0b65
NG-MinG <wgmin.it@outlook.com>
2023-11-14 05:13:47 +0700

    remove http-only

--- backend/common/controllers/auth.controller.ts
+++ backend/common/controllers/auth.controller.ts
@@ -67,7 +67,7 @@ class AuthController implements IController {
         const accessToken = await JsonWebToken.createToken({_id: user.id}, {expiresIn: process.env.JWT_ACCESS_EXPIRES})
         res.cookie('jwt', accessToken, {
             expires: new Date(Date.now() + Number(process.env.JWT_ACCESS_EXPIRES)), // Cookie expiration time in milliseconds
-            httpOnly: true, // Make the cookie accessible only through HTTP
+            // httpOnly: true, // Make the cookie accessible only through HTTP
             secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // Ensure that the cookie is secure in a production environment
         });
 
@@ -99,7 +99,7 @@ class AuthController implements IController {
         
         res.cookie('jwt', accessToken, {
             expires: new Date(Date.now() + Number(process.env.JWT_ACCESS_EXPIRES)), // Cookie expiration time in milliseconds
-            httpOnly: true, // Make the cookie accessible only through HTTP
+            // httpOnly: true, // Make the cookie accessible only through HTTP
             secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // Ensure that the cookie is secure in a production environment
           });
 
@@ -115,7 +115,7 @@ class AuthController implements IController {
         const accessToken = await JsonWebToken.createToken({_id: req.user?.id}, {expiresIn: process.env.JWT_ACCESS_EXPIRES})
         res.cookie('jwt', accessToken, {
             expires: new Date(Date.now() + Number(process.env.JWT_ACCESS_EXPIRES)), // Cookie expiration time in milliseconds
-            httpOnly: true, // Make the cookie accessible only through HTTP
+            // httpOnly: true, // Make the cookie accessible only through HTTP
             secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // Ensure that the cookie is secure in a production environment
           });
         res.redirect(`${process.env.CLIENT_HOST}/auth/login/?u_id=${req.user?.id}`)
