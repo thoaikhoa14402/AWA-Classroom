f4a985b4d503259a7c634e4169d820da1c036ef8
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2023-11-24 23:41:55 +0700

    feat: Lock/ban lecturer account

--- servers/admin/common/controllers/index.ts
+++ servers/admin/common/controllers/index.ts
@@ -2,3 +2,5 @@
 export {default as AuthController} from "./auth.controller";
 export {default as UserProfile} from "./user.controller";
 export {default as StudentController} from "./student.controller";
+export {default as LecturerController} from "./lecturer.controller";
+
