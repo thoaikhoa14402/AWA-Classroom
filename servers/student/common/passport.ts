417934b0e8b871d39e055361333713b551d76837
NG-MinG <wgmin.it@outlook.com>
2023-12-07 15:37:40 +0700

    redirect to https

--- servers/student/common/passport.ts
+++ servers/student/common/passport.ts
@@ -41,7 +41,7 @@ const googleStrategy = new GoogleStrategy({
      // options for Google Strategy
      clientID: process.env.CLIENT_ID_GOOGLE as string,
      clientSecret: process.env.CLIENT_SECRET_GOOGLE as string,
-     callbackURL: "/v1/auth/google/cb",
+     callbackURL: "https://awa-classroom-student-api.vercel.app/v1/auth/google/cb",
      proxy: true,
     }, async (accessToken: string, refreshToken: string, profile: any, done) => {    // Check if user already exists in our database
     let user = await UserModel.findOne({googleID: profile.id})
