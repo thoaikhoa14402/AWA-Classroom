1a79dc1326b905e3e41de05958ca8a67f0bd7e02
NG-MinG <wgmin.it@outlook.com>
2023-12-17 20:41:04 +0700

    options socket

--- clients/student/src/utils/socket.ts
+++ clients/student/src/utils/socket.ts
@@ -3,4 +3,5 @@ import io from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 export const socket = io(process.env.REACT_APP_NOTIFY_HOST as string, {
     autoConnect: false,
+    transports: ['polling','websocket']
 });
