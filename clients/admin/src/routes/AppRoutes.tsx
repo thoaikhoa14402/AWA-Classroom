13b3ef7dd178aa967eac85a061590e97b235ea91
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2024-01-01 18:33:31 +0700

    feat: Create 404 pages for undeveloped pages [UI]

--- clients/admin/src/routes/AppRoutes.tsx
+++ clients/admin/src/routes/AppRoutes.tsx
@@ -3,33 +3,34 @@ import BaseLayout from "~/layouts/BaseLayout";
 import AuthLayout from "~/layouts/AuthLayout";
 import LoginPage from "~/pages/Login";
 import ProtectedRoute from "./ProtectedRoute";
-import DashboardPage from "~/pages/Dashboard";
+// import DashboardPage from "~/pages/Dashboard";
 import StudentManagementPage from "~/pages/StudentManagement";
 import LecturerManagementPage from "~/pages/LecturerManagement";
 import ClassroomManagementPage from "~/pages/ClassroomManagement"
 import ClassroomDetailPage from "~/pages/ClassroomDetailPage"
-import SchedulePage from '~/pages/Schedule';
-import SettingPage from '~/pages/Setting';
+// import SchedulePage from '~/pages/Schedule';
+// import SettingPage from '~/pages/Setting';
 import Profile from "~/pages/Profile";
 import ResetPassword from "~/pages/ResetPassword";
+import Error404 from "~/components/Error404";
 
 
 const AppRoutes = () => {
     return (
         <Routes>
              {/* Protected routes */}
-             <Route element={<BaseLayout />}>
-                <Route path="/dashboard" element={<DashboardPage />}/>
-                <Route path="/classroom-management">
-                    <Route index element = {<ClassroomManagementPage/>}/>
-                    <Route path = ":slug" element = {<ClassroomDetailPage/>}/>
+             <Route element={<ProtectedRoute auth />}>
+                <Route element={<BaseLayout />}>
+                    <Route path="/classroom-management">
+                        <Route index element = {<ClassroomManagementPage/>}/>
+                        <Route path = ":slug" element = {<ClassroomDetailPage/>}/>
+                    </Route>
+                    <Route path="/lecturer-management" element={<LecturerManagementPage/>}/>
+                    <Route path="/student-management" element={<StudentManagementPage/>}/>
+                    <Route path="/user/profile" element={<Profile />} />
+                    <Route path="/user/reset-password" element={<ResetPassword />} />
+                    <Route path="*" element={<Error404/>}  />
                 </Route>
-                <Route path="/lecturer-management" element={<LecturerManagementPage/>}/>
-                <Route path="/student-management" element={<StudentManagementPage/>}/>
-                <Route path="/schedule" element={<SchedulePage/>}/>
-                <Route path="/settings" element={<SettingPage/>}/>
-                <Route path="/user/profile" element={<Profile />} />
-                <Route path="/user/reset-password" element={<ResetPassword />} />
              </Route>
             {/* Authentication routes */}
             <Route element={<ProtectedRoute />}>
